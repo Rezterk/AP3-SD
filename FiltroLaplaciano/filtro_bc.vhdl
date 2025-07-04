ef33bac2ec31826ebb13ea3bf05bd43f664d1332
Enzo Kretzer <kretzer.enzo60@gmail.com>
2025-07-03 23:52:44 -0300

    Remake do BO
    Nessa branch será testada uma nova arquitetura feita para o Bloco Operativo e Bloco de Controle. No Bloco operativo, diversos componentes permaneceram, mas outros saíram ou mudaram sua lógica de sinais. Já no Bloco de Controle foram ajustados os sinais de Status e Controle modificados pelo BO.

--- FiltroLaplaciano/filtro_bc.vhdl
+++ FiltroLaplaciano/filtro_bc.vhdl
@@ -3,20 +3,22 @@ use ieee.std_logic_1164.all;
 
 entity filtro_bc is
     port(
-        clk                                : in  std_logic;       -- clock (sinal de relógio)
-        rst_a                              : in  std_logic;       -- reset assíncrono ativo em nível alto
-        start                              : in  std_logic;
-        doneIMG, maxDEMUX, validPixel      : in  std_logic;
-        done                               : out std_logic;
-        cEndOri, cEndOut, cDEMUX, cREGCONV : out std_logic;
-        zEndOri, zEndOut, zDEMUX           : out std_logic;
-        escMEM                             : out std_logic
+        clk                        : in  std_logic;       -- clock (sinal de relógio)
+        rst_a                      : in  std_logic;       -- reset assíncrono ativo em nível alto
+        start                      : in  std_logic;
+        doneIMG, validPixel        : in  std_logic;
+        done                       : out std_logic;
+        lerMEM, escMEM, sMemEnd    : out std_logic;
+        cP1, cP2, cP3, cP4, cP5    : out std_logic;
+        cEndOri, cEndOut, cREGCONV : out std_logic;
+        zEndOri, zEndOut           : out std_logic;
+        opADDRESS                  : out std_logic_vector(2 downto 0)
     );
 end entity;
 -- Não altere o nome da entidade nem da arquitetura!
 
 architecture behavior of filtro_bc is
-    type state is (S0, S1, S2, S3, S4, S5, S6, S7, S8);
+    type state is (S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13);
     signal current_state, next_state : state;
 begin
     reg_state : PROCESS(clk, rst_a)
@@ -28,43 +30,51 @@ begin
         END IF;
     END PROCESS reg_state;
 
-    lpe : PROCESS(current_state, start, doneIMG, validPixel, maxDEMUX)
+    lpe : PROCESS(current_state, start, doneIMG, validPixel)
     BEGIN
         CASE (current_state) IS
-            WHEN S0 =>
-                IF start = '1' THEN
-                    next_state <= S2;
-                ELSE
-                    next_state <= S0;
-                END IF;
-            WHEN S1 =>
-                IF doneIMG = '1' THEN 
-                    next_state <= S8;
-                ELSE 
-                    next_state <= S2; 
-                END IF;
-            WHEN S2 =>
-                IF validPixel = '1' THEN
-                    next_state <= S3;
-                ELSE
+        	when S0 =>
+        		if start = '1' then
                     next_state <= S1;
-                END IF;
-            WHEN S3 =>
-                IF maxDEMUX = '1' THEN 
-                    next_state <= S5;
-                ELSE 
+                else
+                    next_state <= S0;
+                end if;
+        	when S1 =>
+        		next_state <= S2;
+        	when S2 =>
+        		if doneIMG = '1' then
+                    next_state <= S13;
+                else
+                    next_state <= S3;
+                end if;
+        	when S3 =>
+        		if validPixel = '1' then
                     next_state <= S4;
-                END IF;
-            WHEN S4 =>
-                next_state <= S3;
-            WHEN S5 =>
-                next_state <= S6;
-            when S6 =>
-                next_state <= S7;
-            when S7 =>
-                next_state <= S1;
-            when S8 =>
-            	next_state <= S8;
+                else
+                    next_state <= S12;
+                end if;
+        	when S4 =>
+        		next_state <= S5;
+        	when S5 =>
+        		next_state <= S6;
+        	when S6 =>
+        		next_state <= S7;
+        	when S7 =>
+        		next_state <= S8;
+        	when S8 =>
+        		next_state <= S9;
+        	when S9 =>
+        		next_state <= S10;
+        	when S10 =>
+        		next_state <= S11;
+        	when S11 =>
+        		next_state <= S12;
+        	when S12 =>
+        		next_state <= S2;
+            when S13 =>
+                next_state <= S13;
+            when others =>
+                next_state <= S0;
         END CASE;
     END PROCESS lpe;
 
@@ -72,95 +82,245 @@ begin
     BEGIN
         CASE (current_state) IS
             WHEN S0 =>
-                cEndOri  <= '1';
-                zEndOri  <= '1';
-                cEndOut  <= '1';
-                zEndOut  <= '1';
-                cDEMUX   <= '1';
-                zDEMUX   <= '1';
-                cREGCONV <= '0';
-                escMEM   <= '0';
-                done     <= '0';
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '-';
+                escMEM    <= '0';
+                lerMEM    <= '0';
+                opADDRESS <= "---";
+                done      <= '0';
             WHEN S1 =>
-                cEndOri  <= '1';
-                zEndOri  <= '0';
-                cEndOut  <= '0';
-                zEndOut  <= '-';
-                cDEMUX   <= '0';
-                zDEMUX   <= '-';
-                cREGCONV <= '0';
-                escMEM   <= '0';
-                done     <= '0';
+                cEndOri   <= '1';
+                zEndOri   <= '1';
+                cEndOut   <= '1';
+                zEndOut   <= '1';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '-';
+                escMEM    <= '0';
+                lerMEM    <= '0';
+                opADDRESS <= "---";
+                done      <= '0';
             WHEN S2 =>
-                cEndOri  <= '0';
-                zEndOri  <= '-';
-                cEndOut  <= '0';
-                zEndOut  <= '-';
-                cDEMUX   <= '0';
-                zDEMUX   <= '-';
-                cREGCONV <= '0';
-                escMEM   <= '0';
-                done     <= '0';
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '-';
+                escMEM    <= '0';
+                lerMEM    <= '0';
+                opADDRESS <= "---";
+                done      <= '0';
             WHEN S3 =>
-                cEndOri  <= '0';
-                zEndOri  <= '-';
-                cEndOut  <= '0';
-                zEndOut  <= '-';
-                cDEMUX   <= '0';
-                zDEMUX   <= '-';
-                cREGCONV <= '0';
-                escMEM   <= '0';
-                done     <= '0';
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '-';
+                escMEM    <= '0';
+                lerMEM    <= '0';
+                opADDRESS <= "---";
+                done      <= '0';
             WHEN S4 =>
-                cEndOri  <= '0';
-                zEndOri  <= '0';
-                cEndOut  <= '0';
-                zEndOut  <= '0';
-                cDEMUX   <= '1';
-                zDEMUX   <= '0';
-                cREGCONV <= '0';
-                escMEM   <= '0';
-                done     <= '0';
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '0';
+                cP1       <= '1';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '0';
+                escMEM    <= '0';
+                lerMEM    <= '1';
+                opADDRESS <= "000";
+                done      <= '0';
             WHEN S5 =>
-                cEndOri  <= '1';
-                zEndOri  <= '0';
-                cEndOut  <= '0';
-                zEndOut  <= '0';
-                cDEMUX   <= '1';
-                zDEMUX   <= '1';
-                cREGCONV <= '1';
-                escMEM   <= '0';
-                done     <= '0';
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '1';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '0';
+                escMEM    <= '0';
+                lerMEM    <= '1';
+                opADDRESS <= "001";
+                done      <= '0';
             WHEN S6 =>
-                cEndOri  <= '0';
-                zEndOri  <= '-';
-                cEndOut  <= '0';
-                zEndOut  <= '-';
-                cDEMUX   <= '0';
-                zDEMUX   <= '-';
-                cREGCONV <= '0';
-                escMEM   <= '1';
-                done     <= '0';
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '1';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '0';
+                escMEM    <= '0';
+                lerMEM    <= '1';
+                opADDRESS <= "010";
+                done      <= '0';
             when S7 =>
-                cEndOri  <= '0';
-                zEndOri  <= '0';
-                cEndOut  <= '1';
-                zEndOut  <= '0';
-                cDEMUX   <= '0';
-                zDEMUX   <= '0';
-                cREGCONV <= '0';
-                escMEM   <= '0';
-                done     <= '0';
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '1';
+                cP5       <= '0';
+                sMemEnd   <= '0';
+                escMEM    <= '0';
+                lerMEM    <= '1';
+                opADDRESS <= "011";
+                done      <= '0';
             when S8 =>
-                cEndOri  <= '0';
-                zEndOri  <= '0';
-                cEndOut  <= '0';
-                zEndOut  <= '0';
-                cDEMUX   <= '0';
-                zDEMUX   <= '0';
-                cREGCONV <= '0';
-                escMEM   <= '0';
-                done     <= '1';
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '1';
+                sMemEnd   <= '0';
+                escMEM    <= '0';
+                lerMEM    <= '1';
+                opADDRESS <= "100";
+                done      <= '0';
+            when S9 =>
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '1';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '0';
+                escMEM    <= '0';
+                lerMEM    <= '0';
+                opADDRESS <= "---";
+                done      <= '0';
+            when S10 =>
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '0';
+                escMEM    <= '1';
+                lerMEM    <= '0';
+                opADDRESS <= "---";
+                done      <= '0';
+            when S11 =>
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '1';
+                zEndOut   <= '0';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '0';
+                escMEM    <= '0';
+                lerMEM    <= '0';
+                opADDRESS <= "---";
+                done      <= '0';
+            when S12 =>
+                cEndOri   <= '1';
+                zEndOri   <= '0';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '0';
+                escMEM    <= '0';
+                lerMEM    <= '0';
+                opADDRESS <= "---";
+                done      <= '0';
+            when S13 =>
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '-';
+                escMEM    <= '0';
+                lerMEM    <= '0';
+                opADDRESS <= "---";
+                done      <= '1';
+            when others =>
+                cEndOri   <= '0';
+                zEndOri   <= '-';
+                cEndOut   <= '0';
+                zEndOut   <= '-';
+                cREGCONV  <= '0';
+                cP1       <= '0';
+                cP2       <= '0';
+                cP3       <= '0';
+                cP4       <= '0';
+                cP5       <= '0';
+                sMemEnd   <= '-';
+                escMEM    <= '0';
+                lerMEM    <= '0';
+                opADDRESS <= "---";
+                done      <= '0';
         END CASE;
     END PROCESS ls;
 end architecture;
